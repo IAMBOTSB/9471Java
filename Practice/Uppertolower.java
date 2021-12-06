08480db132af04ad1fffc3ffa378a13a975cc305
IAMBOTSB <36182266+IAMBOTSB@users.noreply.github.com>
2021-12-06 12:22:12 +0530

    Create Uppertolower.java

---
+++ Practice/Uppertolower.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Uppertolower {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a String : ");
+        String str = sc.nextLine();
+
+        System.out.println(str.toLowerCase());
+    }
+}
