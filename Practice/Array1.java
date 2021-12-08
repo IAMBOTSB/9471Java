23107593bf440e144512af157baa84bde02dd21b
IAMBOTSB <36182266+IAMBOTSB@users.noreply.github.com>
2021-12-09 03:22:10 +0530

    Create Array1.java

---
+++ Practice/Array1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Array1{
+    public static void main(String[] args){
+        int[] my_array1 = {23,43,45,12,21,42};
+
+        String[] my_array2 = {"And","cab","pan","dog"};
+
+        System.out.println("Sorted numeric array : "+Arrays.toString(my_array1));
+
+        System.out.println("Sorted string array : "+Arrays.toString(my_array2));
+    }
+}
